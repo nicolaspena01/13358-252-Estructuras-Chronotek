d81e021a27affd63c64f26edd01ae4605059171d
DennisUniCa <dennis.franco01@unicatolica.edu.co>
2025-08-19 20:03:34 -0500

    Update triangulo.java

--- session_2/areas/triangulo.java
+++ session_2/areas/triangulo.java
@@ -1,5 +1,21 @@
-class variable{
-	public static void main(String args[]){
-		
-	}
+import java.util.Scanner;
+
+class AreasFiguras {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Área del Triángulo
+        System.out.println("=== Área de un Triángulo ===");
+        System.out.print("Ingrese la base: ");
+        double base = sc.nextDouble();
+        System.out.print("Ingrese la altura: ");
+        double altura = sc.nextDouble();
+        double areaTriangulo = (base * altura) / 2;
+        System.out.println("El área del triángulo es: " + areaTriangulo);
+
+        sc.close();
+    }
 }
+
+
