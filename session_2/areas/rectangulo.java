48878b85622cdbe163dface693fc6b18782f1bf8
DennisUniCa <dennis.franco01@unicatolica.edu.co>
2025-08-19 20:02:52 -0500

    Update rectangulo.java

--- session_2/areas/rectangulo.java
+++ session_2/areas/rectangulo.java
@@ -1,5 +1,21 @@
-class variable{
-	public static void main(String args[]){
+import java.util.Scanner;
+
+class AreasFiguras {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 		
-	}
+        // Área del Rectángulo
+        System.out.println("\n=== Área de un Rectángulo ===");
+        System.out.print("Ingrese la base: ");
+        double baseRect = sc.nextDouble();
+        System.out.print("Ingrese la altura: ");
+        double alturaRect = sc.nextDouble();
+        double areaRectangulo = baseRect * alturaRect;
+        System.out.println("El área del rectángulo es: " + areaRectangulo);
+
+        sc.close();
+    }
 }
+
+
