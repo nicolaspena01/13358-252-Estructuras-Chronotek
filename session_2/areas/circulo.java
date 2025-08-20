f0c962773d31e1e5db7adeda18ad6fc9cbe5be40
Nicolas <nicolas.pena01@unicatolica.edu.co>
2025-08-19 20:05:35 -0500

    areas

--- session_2/areas/circulo.java
+++ session_2/areas/circulo.java
@@ -1,5 +1,19 @@
-class variable{
-	public static void main(String args[]){
-		
-	}
+import java.util.Scanner;
+
+class AreasCirculo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Área del Círculo
+        System.out.println("\n=== Área de un Círculo ===");
+        System.out.print("Ingrese el radio: ");
+        double radio = sc.nextDouble();
+        double areaCirculo = Math.PI * Math.pow(radio, 2);
+        System.out.println("El área del círculo es: " + areaCirculo);
+
+        sc.close();
+    }
 }
+
+
--- session_2/areas/cuadrado.java
+++ session_2/areas/cuadrado.java
@@ -1,5 +1,19 @@
-class variable{
-	public static void main(String args[]){
-		
-	}
+import java.util.Scanner;
+
+class AreasCuadrado {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Área del Cuadrado
+        System.out.println("\n=== Área de un Cuadrado ===");
+        System.out.print("Ingrese el lado: ");
+        double lado = sc.nextDouble();
+        double areaCuadrado = lado * lado;
+        System.out.println("El área del cuadrado es: " + areaCuadrado);
+
+        sc.close();
+    }
 }
+
+
