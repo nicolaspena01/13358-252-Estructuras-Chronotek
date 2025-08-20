6cc710a36dc6ab1dcc77f1dffa77fcda0fa21f51
Nicolas <nicolas.pena01@unicatolica.edu.co>
2025-08-19 19:53:00 -0500

    archivos area

---
+++ session_2/areas/rectangulo.java
@@ -0,0 +1,5 @@
+class variable{
+	public static void main(String args[]){
+		
+	}
+}
--- session_2/variables.java
+++ session_2/variables.java
@@ -1,5 +1,20 @@
 class variable{
 	public static void main(String args[]){
-	System.out.println("Hello word");	
+		System.out.println("Hello word");	
+
+		byte tamanio = 50;
+		short superfie = 1203;
+		long distance = 484848483L;
+		boolean isActive = false;
+		float salary = 14502304F;
+		int year = 2025;
+		double pi = 3.1448558844;
+
+		System.out.println("EL valor de pi es "+tamanio);
 	}	
 }
+
+triangulo
+cuadrado
+reptangulo
+circulo
